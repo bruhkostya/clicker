3df970cb46bb78e72761faa746838469fcd836ca
bruhkostya <bruhkostya@gmail.com>
2023-05-19 16:26:05 +0300

    My first Git

---
+++ clicker/Assets/scripts/Code.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Code : MonoBehaviour
+{
+    [SerializeField] int money;
+    public Text moneyText;
+
+    public void ButtonClick(){
+    money++;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        moneyText.text = money.ToString();
+    }
+}
